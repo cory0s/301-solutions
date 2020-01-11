b61bb602166cc410276c6b79ad6301b8945c5922
Cory Henderson <chenderson19899@gmail.com>
2020-01-11 13:56:05 -0800

    final

--- lab-02/js/app.js
+++ lab-02/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Declare global vars
+let horns = [];
 let keywords = [];
 let currentPage = 'page-1';
 
@@ -21,24 +22,39 @@ function renderCreatures(arr){
 }
 
 // Event handler for keyword selection
-$('select').on('change', function(e){
+$('select[class="keywords"]').on('change', function(e){
     $('div').hide();
     $(`div[class="${e.target.value}"]`).show();
-})
+});
 
+$('select[class="sort"]').on('change', function(e){
+  if(e.target.value === 'name'){
+    horns.sort((a,b) => a.name.localeCompare(b.name));
+    renderCreatures(horns);
+  } else {
+    horns.sort((a,b) => a.horns - b.horns);
+    renderCreatures(horns);
+  }
+});
+
+// Pagination event listener
 $('button').on('click', function(){
   if(this.id === 'previous-button'){
     currentPage = 'page-1';
   } else {
     currentPage = 'page-2';
   }
+
+  $('select[class="sort"] option').prop('selected', function() {
+    return this.defaultSelected;
+  });
   
   run();
-})
+});
 
 // Get data, create creatures, update keywords, render
 function run(){
-  const horns = [];
+  horns = [];
   keywords = [];
     getData()
         .then(data => data.forEach(obj => horns.push(new Creature(obj))))
